0017f0d30ee70a702df524e02b6519889805fa5b
Samridda Paudel <84239594+samriddaxdev@users.noreply.github.com>
2024-06-08 15:14:11 +0545

    styling the components

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -15,7 +15,13 @@ export default function SectionWrapper(props) {
           {title[2]}
         </h2>
       </div>
-      {children}
+
+      <div
+        className='max-w-[800px] w-full flex flex-col
+        mx-auto gap-10 '
+      >
+        {children}
+      </div>
     </section>
   );
 }
