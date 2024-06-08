0dab9137125d161a73df7ed68274a268f083df40
Samridda Paudel <84239594+samriddaxdev@users.noreply.github.com>
2024-06-08 10:41:24 +0545

    styling up a bit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,22 +4,28 @@ export default function Hero() {
   return (
     <div
       className='min-h-screen flex flex-col gap-10
-      items-center justify-center'
+      items-center justify-center text-center max-w-[900px] 
+      w-full mx-auto'
     >
-      <p>IT'S TIME TO GET</p>
-      <h1
-        className='uppercase font-semibold
+      <div className='flex flex-col gap-4'>
+        <p>IT'S TIME TO GET</p>
+        <h1
+          className='uppercase font-semibold
       text-4xl sm:text-5xl md:text-6xl
       lg:text-7xl'
-      >
-        Lift<span className='text-blue-400'>Logix</span>
-      </h1>
+        >
+          Lift<span className='text-blue-400'>Logix</span>
+        </h1>
+      </div>
       <p className='text-sm md:text-base font-light'>
         I hereby acknowledge that I may become{' '}
         <span className='text-blue-400 font-medium'>unbelievably fit </span> and
         accept all risks of becoming the local<span> mass montrosity</span>,
         afflicted with severe body dismorphia, unable to fit through doors.
       </p>
+      <button className='px-8 py-4 rounded'>
+        <p>Accept and Begin</p>
+      </button>
     </div>
   );
 }
