c620784c2444339df1bbd88ed499f77dc1f6e10c
Samridda Paudel <84239594+samriddaxdev@users.noreply.github.com>
2024-06-08 16:48:43 +0545

    styling the exercises buttons

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import SectionWrapper from './SectionWrapper';
+import { SCHEMES, WORKOUTS } from '../utils/soldier';
 
 function Header(props) {
   const { index, title, description } = props;
   return (
     <div className='flex flex-col gap-4'>
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center justify-center gap-2'>
         <p
           className='text-3xl sm:text-4xl md:text-5xl
           font-semibold text-slate-400'
         >
           {index}
         </p>
-        <h4 className='text-lg sm:text-2xl md:text-3xl'>{title}</h4>
+        <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
       </div>
       <p className='text-sm sm:text-base mx-auto'>{description}</p>
     </div>
@@ -30,6 +31,19 @@ export default function Generator() {
         title={'Pick your poison'}
         description={'Select the workout you wish to endure'}
       />
+      <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+              className='bg-slate-950 border border-blue-400
+              py-4 rounded-lg'
+              key={typeIndex}
+            >
+              <p>{type}</p>
+            </button>
+          );
+        })}
+      </div>
     </SectionWrapper>
   );
 }
